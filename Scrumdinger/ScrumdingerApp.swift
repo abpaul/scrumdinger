8276d34f5ca82d923e8014233687de7682ae6e68
Paul AB <paul+github@badarau.net>
2023-02-04 18:55:06 +0200

    Initial

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,19 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: $scrums)
+            }
+        }
+    }
+}
